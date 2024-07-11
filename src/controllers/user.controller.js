26c3950291db0e0025b242274e5ffe9f9273d7aa
mdsiaofficial <mdsiaofficial@gmail.com>
2024-07-11 17:13:24 +0600

    "Added new controller methods and routes for user management, including getting watch history, changing password, updating account details, and updating avatar and cover image."

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 
 const generateAccessAndRefreshTokens = async (userId) => {
@@ -431,7 +432,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     }
   ])
 
-  if (!channel?.length){
+  if (!channel?.length) {
     throw new ApiError(404, "Channel does not exists");
 
 
@@ -441,6 +442,52 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, channel[0], "User channel fetched succesfully"));
 })
+
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first:"owner",
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+})
+
 export {
   registerUser,
   loginUser,
